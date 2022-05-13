d9b3db1d38d6dbf4096a858d25f0edf54f7abc1c
Umasekaran63 <umasekaran63@gmail.com>
2022-05-13 20:39:34 +0530

    Build is ready

---
+++ src/org/test/JavaGit.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class JavaGit {
+	public static void main(String[] args) {
+		int a=100,b=20;
+		if(a<b) {
+			System.out.println("the value of a is:"+a);
+		}
+		else {
+		System.out.println("the value of b is:"+b);
+	}
+ }
+}
